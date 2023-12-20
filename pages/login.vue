bd79ef833c41bcb149a98af8d87007a62bffae72
liqiang <3556046510@qq.com>
2023-12-20 19:35:54 +0800

    login

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+123456789
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created() {
+
+  },
+}
+</script>
+<style scoped>
+
+</style>
